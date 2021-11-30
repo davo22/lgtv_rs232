248a3269341120f158ba5b933b0442a142a909f2
Dawid Rashid <dawid@kitopi.com>
2021-11-30 17:10:01 +0100

    chore: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='lgtv_rs232',
     packages=find_packages(include=['lgtv_rs232']),
-    version='0.1.0',
+    version='0.2.0',
     description='LG TV RS-232 client.',
     author='Dawid Rashid',
     license='MIT',
